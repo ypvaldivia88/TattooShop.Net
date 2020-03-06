776c563f8431aa4178aa7ba06c0c5c9a6568200a
Yasmani Palmero <yasmany.07@gmail.com>
2020-03-06 12:01:27 -0500

    Agregar archivos de proyecto.

---
+++ TattooShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TattooShop.MvcApplication" Language="C#" %>
---
+++ TattooShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TattooShop.Startup))]
+namespace TattooShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
